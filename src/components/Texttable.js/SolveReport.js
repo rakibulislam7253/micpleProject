8d4d5b58d5708253d133b9623acfec8480065772
unknown <altabrakib7@gmail.com>
2023-02-13 11:39:02 +0600

    problem-1

--- src/components/Texttable.js/SolveReport.js
+++ src/components/Texttable.js/SolveReport.js
@@ -1,36 +1,165 @@
-import React from 'react';
-import { BiBorderRadius } from 'react-icons/bi';
-import demo2 from '../demo/1580007.jpg'
-const SolveReport = () => {
+import React, { useState } from "react";
+
+const Problem1 = () => {
+  const [show, setShow] = useState("all");
+  const [table, settable] = useState([])
+  const [activedata, setActivedata] = useState([])
+  const [completed, setCompleted] = useState([])
+  const [name, setName] = useState('');
+  const [status, setStatus] = useState('');
+
+  // ******************************* //
+  const handleClick = (val) => {
+    if (val === "active") {
+      const filterData = table.filter(item => item.status.includes('active'))
+      setActivedata(filterData)
+      // console.log(activedata)
+    }
+
+
+    else if (val === "completed") {
+      const filterData = table.filter(item => item.status.includes('completed'))
+      setCompleted(filterData)
+      console.log(completed)
+    }
+
+
+    setShow(val);
+  };
+
+  // ******************************* //
+  function handelsubmit(e) {
+    e.preventDefault();
+    settable([...table, { name, status }]);
+
+    setName('');
+    setStatus('');
+
+    console.log(table);
+
+  }
+
+
+
   return (
-    <div style={{ textAlign: "left", display: "flex", alignContent: "center", justifyContent: "space-between", padding: "0px 15px", backgroundColor: "Lightgray", width: "33%", height: "100%" }}>
-      <div style={{ display: "flex" }}>
 
-        <div style={{ marginTop: "2px" }}>
-          <img style={{ width: "90px", height: "90px" }} src="https://img.icons8.com/plasticine/256/user-male-circle.png" />
+
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+        <div className="col-6 ">
+
+          <form onSubmit={handelsubmit} className="row gy-2 gx-3 align-items-center mb-4">
+
+            <div className="col-auto">
+
+              <input type="text" value={name} onChange={e => setName(e.target.value)} className="form-control" placeholder="Name" />
+            </div>
+
+            <div className="col-auto">
+              <input type="text" value={status} onChange={e => setStatus(e.target.value)} className="form-control" placeholder="Status" />
+            </div>
+
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
+
+          </form>
+
+
         </div>
+        <div className="col-8">
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "all" && "active"}`}
+                type="button"
+                onClick={() => handleClick("all")}
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "active" && "active"}`}
+                type="button"
+                onClick={() => handleClick("active")}
+              >
+                Active
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "completed" && "active"}`}
+                type="button"
+                onClick={() => handleClick("completed")}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
 
-        <div style={{ padding: "2px 5x", marginTop: "2px", marginLeft: "5px",width:"50%" }}>
-          <h4><b>John Doe</b></h4>
-          <p style={{ marginTop: "-20px" }}>Workes doesn't add yet.
-            Educations doesn't add yet.</p>
+
+
+
+
+
+          <div className="tab-content"></div>
+          <table className="table table-striped ">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+              </tr>
+              {
+                table.map(item => (
+
+
+                  <tr>
+
+                    <td scope="col">{item.name}</td>
+                    <td scope="col">{item.status}</td>
+                  </tr>
+
+                ))
+
+              }
+
+              {
+                activedata.map(item => (
+
+
+                  <tr>
+
+                    <td scope="col">{item.name}</td>
+                    <td scope="col">{item.status}</td>
+                  </tr>
+
+                ))
+              }
+              
+              {
+                completed.map(item => (
+
+
+                  <tr>
+
+                    <td scope="col">{item.name}</td>
+                    <td scope="col">{item.status}</td>
+                  </tr>
+
+                ))
+              }
+
+
+
+            </thead>
+            <tbody></tbody>
+          </table>
         </div>
       </div>
-
-      <div style={{ marginTop: "30px", marginLeft: "-10px" }}>
-
-        <button style={{ margin: "10px", padding: "3px 10px" }}>Unfollow</button>
-        <button style={{ padding: "3px 10px" }}>Block</button>
-      </div>
-
-
     </div>
-
-
   );
 };
 
-export default SolveReport;
-
-
-
+export default Problem1;
